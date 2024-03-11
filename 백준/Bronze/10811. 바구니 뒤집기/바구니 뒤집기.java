6a05da4033c9b68c50381648b660fc3b89f865e0
j3261221 <128676068+j3261221@users.noreply.github.com>
2024-03-11 23:03:06 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static String input;
+    static int[] arr;
+    static StringTokenizer st;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        List<Integer> copy = new ArrayList<>();
+
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int I = Integer.parseInt(st.nextToken()) - 1;
+            int J = Integer.parseInt(st.nextToken()) - 1;
+            int K = 0;
+            copy.clear();
+            for (int j = J; j >= I; j--) {
+                copy.add(arr[j]);
+            }
+            for (int j = I; j <= J; j++) {
+                arr[j] = copy.get(K++);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
