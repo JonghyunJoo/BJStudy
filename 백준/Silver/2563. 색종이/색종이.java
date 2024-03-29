94cc6772b8dde7fbb9cf0e934557472cf3f56be2
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2024-03-30 00:06:36 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,26 +1,29 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int total = 0;  //검은 영역의 넓이
-        int n = Integer.parseInt(br.readLine());  //색종이 개수
-        boolean[][] arr = new boolean[101][101];  //도화지
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            //(x,y)부터 (x+9, y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해준다
-            for (int j = x; j < x+10; j++) {
-                for (int k = y; k < y+10; k++) {
-                    if (!arr[j][k]) {
-                        arr[j][k] = true;
-                        total++;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+        int x = 0;
+        int y = 0;
+        int totalArea = 0;
+        boolean[][] paper = new boolean[100][100];
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            x = Integer.parseInt(stringTokenizer.nextToken());
+            y = Integer.parseInt(stringTokenizer.nextToken());
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        totalArea++;
                     }
                 }
+
             }
         }
-        System.out.print(total);
+        System.out.println(totalArea);
     }
 }
