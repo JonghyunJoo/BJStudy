72fc1b03369673c2b286901b4fe0b17ed9e5e5ec
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2024-05-11 13:05:34 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 172 ms, Memory: 23700 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        bw.write("<");
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < K - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            bw.write(queue.poll() + "");
+            if (!queue.isEmpty()) {
+                bw.write(", ");
+            }
+        }
+        bw.write(">\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
