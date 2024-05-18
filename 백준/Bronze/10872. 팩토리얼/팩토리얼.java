466f09d862bc169e6656fe9e326eea40f3dd786f
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2024-05-18 23:17:57 +0900

    [Bronze III] Title: 팩토리얼, Time: 140 ms, Memory: 15796 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int fac = 1;
+
+        if (N != 0) {
+            for (int i = 1; i <= N; i++) {
+                fac *= i;
+            }
+        }
+        bw.write(fac + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
