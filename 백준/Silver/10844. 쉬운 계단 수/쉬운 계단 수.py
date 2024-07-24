37819e687d9bbfb569dd77ff31bd8460601469d4
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2024-07-24 21:27:44 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,20 @@
+def stair_number(N):
+    dp = [[0] * 10 for _ in range(N + 1)]
+
+    for i in range(1, 10):
+        dp[1][i] = 1
+
+    for i in range(2, N + 1):
+        for j in range(10):
+            if j > 0:
+                dp[i][j] += dp[i - 1][j - 1]
+            if j < 9:
+                dp[i][j] += dp[i - 1][j + 1]
+            dp[i][j] %= 1000000000
+
+    result = sum(dp[N]) % 1000000000
+    return result
+
+
+N = int(input())
+print(stair_number(N))
