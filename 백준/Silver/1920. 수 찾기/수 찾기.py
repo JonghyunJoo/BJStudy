54eb661cc50e1733514bd01c36f046b2819331bc
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2024-08-25 23:52:47 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 50380 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,11 @@
+N = int(input()) 
+A = set(map(int, input().split())) 
+
+M = int(input())
+queries = list(map(int, input().split())) 
+
+for x in queries:
+    if x in A:
+        print(1)
+    else:
+        print(0)
