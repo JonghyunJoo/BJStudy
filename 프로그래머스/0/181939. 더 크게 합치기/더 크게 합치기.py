4478bd1c4e23ffc1afb76d644bdfe8afe3390f78
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2025-03-01 05:09:04 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,7 +1,2 @@
 def solution(a, b):
-    n1 = int(str(a)+str(b))
-    n2 = int(str(b)+str(a))
-    if n1>n2:
-        return n1
-    else:
-        return n2
+    return max(int(f"{a}{b}"), int(f"{b}{a}"))
