31cead0e260d26d377009f3db246f1186aedc705
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2025-03-15 08:19:23 +0900

    [level 0] Title: 2의 영역, Time: 1.77 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -3,6 +3,6 @@ def solution(arr):
         return [-1]
     
     i1 = arr.index(2)
-    i2 = len(arr) - 1 - arr[::-1].index(2)
+    i2 = len(arr) - arr[::-1].index(2)
     
-    return arr[i1:i2+1]
+    return arr[i1:i2]
