44eedb64b813e07c8642491d8d9b2d5bcf3c7ca9
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2025-03-18 13:56:09 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 9.40 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -6,15 +6,6 @@ def solution(mats, park):
     for size in mats:
         for i in range(n - size + 1):
             for j in range(m - size + 1):
-                is_possible = True
-                for x in range(i, i + size):
-                    for y in range(j, j + size):
-                        if park[x][y] != "-1":
-                            is_possible = False
-                            break
-                    if not is_possible:
-                        break
-                if is_possible:
+                if all(park[x][y] == '-1' for x in range(i, i+size) for y in range(j, j+size)):
                     return size
-    
     return -1
