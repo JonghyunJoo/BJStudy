64add278bee949a1ec0b3425418095188011eec2
Jonghyun Joo <128676068+JonghyunJoo@users.noreply.github.com>
2025-03-21 14:08:09 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,12 +1,12 @@
 def solution(today, terms, privacies):
-    t_year,t_month,t_day = list(map(int, today.split(".")))
+    t_year,t_month,t_day = map(int, today.split("."))
     today = month_to_day(year_to_month(t_year) + t_month) + t_day
     
     term_dict = {key: int(value) for key, value in (term.split() for term in terms)}
     answer = []
     for i, privacy in enumerate(privacies):
         p_date, p_term = privacy.split()
-        p_year, p_month, p_day = list(map(int, p_date.split(".")))
+        p_year, p_month, p_day = map(int, p_date.split("."))
         p_date = month_to_day(year_to_month(p_year) + p_month + term_dict[p_term]) + p_day
         
         if p_date <= today:
